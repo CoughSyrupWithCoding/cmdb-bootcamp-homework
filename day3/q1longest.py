5d07ee2527917ac1f5a91c5234c58b9290e443b9
CMDB <mtisza1@jhu.edu>
2014-09-05 08:36:03 -0400

    with revcomp algorythm

--- day3/q1longest.py
+++ day3/q1longest.py
@@ -18,6 +18,16 @@ sorted_list = sorted(sequence_list, key=len, reverse=True)
     #combining the first part of the seq_id with the sequence in order to make them items in a list to be sorted
     
 longest_sequences = sorted_list[:100]
-print len(longest_sequences)
+#here is the list
 
+#print longest_sequences
+test_list = longest_sequences[98:]
+basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+reverse_comp_sequeneces = []
+for i in test_list:
+    rev_seq = "".join[i[::-1]]
+    rev_comp = [basecomplement[b] for b in rev_seq]
+    rev_comp_str = "".join(rev_comp)
+    reverse_comp_sequences.append(rev_comp_str)
 #displays top 100 strings
+print reverse_comp_sequences
